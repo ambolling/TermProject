07394d5f1ee77597feeedc3fcee736b93bc8000c
Anna Bolling <annabolling@annas-air-2.myfiosgateway.com>
2023-11-22 15:56:17 -0500

    Changed person to become a person class

--- main.py
+++ main.py
@@ -19,43 +19,66 @@ def generatePopulation(app):
     for i in range(100):
         xVal = random.randint(app.populationLeftBoundary,app.populationRightBoundary)
         yVal = random.randint(app.populationTopBoundary,app.populationBottomBoundary)
-        app.populationHealthyMembers.append([xVal,yVal])
+        newPerson = person(xVal, yVal, 'healthy', 'green')
+        app.populationHealthyMembers.append(newPerson)
+    
+
+class person():
+    def __init__(self, xVal, yVal, type, color):
+        self.xVal = xVal
+        self.yVal = yVal
+        self.type = type
+        self.color = color
+
+    def changeTypeColor(self, newType, newColor):
+        self.type = newType
+        self.color = newColor
+
+
 
 def redrawAll(app):
     drawPopulation(app)
 
 
 def drawPopulation(app):
-    for pair in app.populationHealthyMembers:
-        drawCircle(pair[0],pair[1],10, fill = 'green')
-    for pair in app.populationInfectedMembers:
-        drawCircle(pair[0],pair[1],10, fill = 'red')
+    for val in app.populationHealthyMembers:
+        drawCircle(val.xVal, val.yVal, 10, fill = val.color)
+    for val in app.populationInfectedMembers:
+        drawCircle(val.xVal, val.yVal, 10, fill = val.color)
 
 def generateInfectedMember(app):
     boardCenterX = (app.populationRightBoundary+app.populationLeftBoundary)//2 
     boardCenterY = (app.populationTopBoundary+app.populationBottomBoundary)//2
     #randomly infect someone in the center of the board 
     for person in app.populationHealthyMembers:
-        if distance(person[0], boardCenterX, person[1],boardCenterY) < 100:
+        xVal = person.xVal
+        yVal = person.yVal
+        if distance(xVal, boardCenterX, yVal,boardCenterY) < 100:
+            person.changeTypeColor('infected','red')
+            app.populationHealthyMembers.remove(person)
             app.populationInfectedMembers.append(person)
             break
+    
 
 def onKeyPress(app, key):
     if key == 'g':
         spreadInfection(app)
 
 def spreadInfection(app):
-    x = [50,50]
-    app.populationInfectedMembers.append(x)
-    for infected in app.populationInfectedMembers:
-        for healthy in app.populationHealthyMembers:
-            if distance(infected[0],infected[1],healthy[0],healthy[1]) < 10:
-                app.populationInfectedMembers.append(healthy)
-                app.populationHealthyMembers.remove(healthy)
+    for infectedPerson in app.populationInfectedMembers:
+        for healthyPerson in app.populationHealthyMembers:
+            if distance(infectedPerson.xVal,healthyPerson.xVal,infectedPerson.yVal,healthyPerson.yVal) <= (100+10):
+                healthyPerson.changeTypeColor('infected','red')
+    for person in app.populationHealthyMembers:
+        if person.type == 'infected':
+            app.populationInfectedMembers.append(person)
+            app.populationHealthyMembers.remove(person)
+
+
 
 
 def distance(x1,x2,y1,y2):
-    return (((x1-x2)**2+(y1-y2)**2)**.5)
+    return ((x2-x1)**2+(y2-y1)**2)**.5
 
 def main():
     runApp(width = 1000, height = 1000)
