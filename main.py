18abd84d8f2ca626fa18aa34ca9e68a698b376f9
Anna Bolling <annabolling@annas-air-2.myfiosgateway.com>
2023-11-25 22:16:29 -0500

    Updating welcome screen

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def onAppStart(app):
     app.populationSizes = dict()
     app.populationSize = 100
     app.reproductionNumber = None
+    app.reproductionNumbers = dict()
 
 
 def generatePopulation(app):
@@ -48,41 +49,54 @@ def main_redrawAll(app):
     drawConnections(app)
 
 def welcome_redrawAll(app):
-    drawLabel("Welcome to Community Immunity", app.width/2, app.height/4, size = 50)
+    drawLabel("Welcome to Community Immunity", app.width/2, app.height/6, size = 50)
     getPathogenParameters(app)
-    selectPopulationSize(app)
+
 
 def drawMainLabels(app):
     pass
 
 def getPathogenParameters(app):
     selectPathogenContagiousLevel(app)
+    selectPopulationSize(app)
+    selectReproductionNumber(app)
 
 def selectPathogenContagiousLevel(app):
-    drawLabel('Select a radius for infection:',app.width/4, app.height/3,size = 25, fill = 'black')
-    drawLine(app.width/2, app.height/3, app.width/2+300, app.height/3,fill = 'grey')
+    drawLabel('Select a radius of infection for your pathogen: ',app.width/4, app.height/4,size = 20, fill = 'black')
+    drawLine((app.width/8)*4, (app.height/4), (app.width/8)*7, app.height/4,fill = 'grey')
 
-    drawLabel('40',app.width/2, app.height/3+30, size = 20)
-    drawCircle(app.width/2, app.height/3, 10, fill = 'red')
-    app.viralRadiusOptions[40]=[app.width/2, app.height/3]
+    drawLabel('40',(app.width/8)*4, app.height/4+25, size = 20)
+    drawCircle((app.width/8)*4, app.height/4, 10, fill = 'red')
+    app.viralRadiusOptions[40]=[(app.width/8)*4, app.height/4+25]
 
-    drawLabel('100',app.width/2+300, app.height/3+30, size = 20)
-    drawCircle(app.width/2+300, app.height/3, 10, fill = 'red')
-    app.viralRadiusOptions[100]=[app.width/2+300, app.height/3]
+    drawLabel('100',(app.width/8)*7, app.height/4+25, size = 20)
+    drawCircle((app.width/8)*7, app.height/4, 10, fill = 'red')
+    app.viralRadiusOptions[100]=[(app.width/8)*4, app.height/4+25]
 
 
 def selectPopulationSize(app):
-    drawLabel('Select a size for your population:',app.width/4, app.height/2,size = 25, fill = 'black')
-    drawLine(app.width/2, app.height/2, app.width/2+300, app.height/2,fill = 'grey')
+    drawLabel('Select a size for your population:',app.width/4, (app.height/8)*3,size = 25, fill = 'black')
+    drawLine((app.width/8)*4, (app.height/8)*3, (app.width/8)*7, (app.height/8)*3,fill = 'grey')
     
-    drawLabel('10',app.width/2, app.height/2+30, size = 20)
+    drawLabel('10',(app.width/8)*4, (app.height/8)*3+25, size = 20)
+    drawCircle((app.width/8)*4, (app.height/8)*3, 10, fill = 'red')
+    app.populationSizes[10] = [(app.width/8)*4, (app.height/8)*3]
+
+    drawLabel('300',(app.width/8)*7, (app.height/8)*3+25, size = 20)
+    drawCircle((app.width/8)*7, (app.height/8)*3, 10, fill = 'red')
+    app.populationSizes[10] = [(app.width/8)*7, (app.height/8)*3]
+
+def selectReproductionNumber(app):
+    drawLabel('Select a reproduction number for your pathogen:',app.width/4, (app.height/2),size = 20, fill = 'black')
+    drawLine(app.width/4, app.height/2,app.width/4+300, (app.height/4)*3,fill = 'grey')
+
+    drawLabel('None',app.width/2, app.height/2+30, size = 20)
     drawCircle(app.width/2, app.height/2, 10, fill = 'red')
-    app.populationSizes[10] = [app.width/2, app.height/2]
+    app.reproductionNumbers[None] = [app.width/2, app.height/2]
 
-    drawLabel('300',app.width/2+300, app.height/2+30, size = 20)
+    drawLabel('10',app.width/2+300, app.height/2+30, size = 20)
     drawCircle(app.width/2+300, app.height/2, 10, fill = 'red')
-    app.populationSizes[300] = [app.width/2+300, app.height/2]
-
+    app.reproductionNumbers[10] = [app.width/2+300, app.height/2]
 
 def welcome_onKeyPress(app, key):
     if key == 'space':
